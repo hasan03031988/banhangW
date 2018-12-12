2cff4b46ad0157df1586095cc5ca46c46d08dcf1
HaSan <hoangsan0303@gmail.com>
2018-12-12 21:58:21 +0900

    the first commit

---
+++ BanHangW.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangW.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BanHangW.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BanHangW.Web.MvcApplication" Language="C#" %>
