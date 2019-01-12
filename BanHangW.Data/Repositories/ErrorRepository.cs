fb8b55f710e02ba012c954c1b88a454c76dec031
HaSan <hoangsan0303@gmail.com>
2019-01-12 23:41:11 +0900

    add web API

---
+++ BanHangW.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using BanHangW.Data.Infrastructure;
+using BanHangW.Model.Models;
+
+namespace BanHangW.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
